b3f5fe458392a3a67dcfc709e9fc3b31e2731431
roldan096 <roldan096@gmail.com>
2019-01-08 21:08:54 +0000

    namas2

--- ParkingManagment/ParkingManagment/settings.py
+++ ParkingManagment/ParkingManagment/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w@7#9nocnem#pdruw5cz@br#j%-_evkib*6he+16bfutg#bpr6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =['*']
 
 
 # Application definition
@@ -81,15 +81,15 @@ WSGI_APPLICATION = 'ParkingManagment.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -100,6 +100,8 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
