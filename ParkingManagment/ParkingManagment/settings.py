520f6778d4f176b3c6f7e55c1c87a7422deef3b8
Rodrigo Roldan <roldan096@gmail.com>
2019-01-09 10:30:40 -0600

    abc2

--- ParkingManagment/ParkingManagment/settings.py
+++ ParkingManagment/ParkingManagment/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w@7#9nocnem#pdruw5cz@br#j%-_evkib*6he+16bfutg#bpr6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
