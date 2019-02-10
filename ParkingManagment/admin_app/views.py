c938f2d9af8d19c1f63cac209d2415475bcbf316
Rodrigo Roldan <roldan096@gmail.com>
2019-02-10 13:40:29 -0600

    user2

--- ParkingManagment/admin_app/views.py
+++ ParkingManagment/admin_app/views.py
@@ -28,7 +28,10 @@ class ExcepcionListView(ListView):
 class SucursalListView(ListView):
     model = Sucursal
     def get_queryset(self):
-        sucursales = Sucursal.objects.filter(supervisor=self.request.user)
+        if(self.request.user.is_superuser):
+            sucursales = Sucursal.objects.all()
+        else:
+            sucursales = Sucursal.objects.filter(supervisor=self.request.user)
         query = self.request.GET.get('s')  
         if query:
             sucursales = sucursales.filter(nombre__icontains=query)
