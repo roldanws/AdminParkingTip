894fb3c038bcddd1fc8996e7eead7561a68b0dfd
Rodrigo Roldan <roldan096@gmail.com>
2019-02-17 21:41:26 -0600

    segunda app suscripcion

--- ParkingManagment/admin_app/views.py
+++ ParkingManagment/admin_app/views.py
@@ -90,7 +90,6 @@ class EstadisticasListView(ListView):
 @method_decorator(staff_member_required, name="dispatch")
 class CorteListView(ListView):
     model = Corte
-    paginate_by = 12
     def get_queryset(self):
         #context['temp'] = self.request.GET.get('temp') 
 
